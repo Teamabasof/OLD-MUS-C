118c3280efdfe9f85703e05fe1c0990b6f3ecf3a
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2023-03-18 00:04:24 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
+SESSION_NAME = getenv("SESSION_NAME", "AgAj0wFPavV5LhgeeK2gu7LiX3bLApjj9Oq42JAQOUodAtPgR_o0DXbd0RPKm0Jsxz5sw49B3hupQyS4OfFwbzbff_BLQb7gEEywlosiHsVELhYnbmhgXIA8QdvwP6lg_QUqx4Eflowz6fxSwpeDKMNCpxNIDXSkgeNr2owRlLJaeGDbyLZFB26_QZEgIus2wkYg6sYayCqzrzYSPegLEybUuilod_6qbaS8D9dXDWVzWRAmM-2PNfQcmqB3kSOqCgDzLI6Y1pRH37VpwAFp2SbiOTX1Jm_E5Mo0CuSbPRQnTMtM5rmvOqSsGeqVg0lZCJajFCCZve_C7CM6umBjfo6FAAAAAUhXc-UA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5681957815:AAFu8TdIl9pYlQjSzm2r8rDL1TQqjxGyzGg")
 BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+API_ID = int(getenv("API_ID",15954332))
+API_HASH = getenv("API_HASH", "85adea6f1eaf068b707703b4846a9ced")
+BOT_USERNAME = getenv("BOT_USERNAME", "AzeSongRobot")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
